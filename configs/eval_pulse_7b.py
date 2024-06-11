a0538e9394a93e9b14f356b7f437b86d2755f41f
QXY <44722826+QXY716@users.noreply.github.com>
2024-06-11 10:41:14 +0800

    Update and rename eval_pulse_7b to eval_pulse_7b.py

--- configs/eval_pulse_7b.py
+++ configs/eval_pulse_7b.py
@@ -2,6 +2,6 @@ from mmengine.config import read_base
 
 with read_base():
     from .datasets.MedBench.medbench_gen import medbench_datasets
-    from .models.pulse.pulse_7b import models
+    from .models.pulse.hf_pulse_7b import models
 
 datasets = [*medbench_datasets]
